919a1895e0d4a948320f39af37fd0adbac1a1859
PC <PC@DESKTOP-P8VNSRI>
2026-07-25 22:08:19 +0300

    Add project files.

---
+++ C#Basic01G02/Assingment2.cs
@@ -0,0 +1,332 @@
+﻿//QUESTION 1
+
+//namespace c_basic01g02
+//{
+//    internal class assingment2
+//    {
+//        static void main()
+//        {
+//            string title = "clean code";
+//            string uppertitle = title.toupper();
+
+//            console.writeline(title);
+//            console.writeline(uppertitle);
+//        }
+//    }
+//}
+
+//QUESTION 2
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string title1 = "Clean Code";
+//            string title2 = "Clean Code";
+
+//            bool sameObject = ReferenceEquals(title1, title2);
+//            Console.WriteLine(sameObject);
+//        }
+//    }
+//}
+
+//QUESTION 3
+
+//using System.Text;
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            StringBuilder sb = new StringBuilder();
+//            sb.Append("Book List");
+//            sb.Append(" - Updated");
+
+//            Console.WriteLine(sb.ToString());
+//        }
+//    }
+//}
+
+//QUESTION 4
+
+//using System.Text;
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            StringBuilder sb = new StringBuilder();
+//            sb.Append("Book List");
+//            sb.Append(" - Updated");
+
+//            sb.Replace("Book List", "Library");
+
+//            Console.WriteLine(sb.ToString());
+//        }
+//    }
+//} 
+
+//QUESTION 5 
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string title = "Clean Code";
+//            int pages = 464;
+
+//            string sentence = "Book: " + title + ", Pages: " + pages;
+
+//            Console.WriteLine(sentence);
+//        }
+//    }
+//}
+
+//QUESTION 6
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string title = "Clean Code";
+//            int pages = 464;
+
+//            string sentence = $"Book: {title}, Pages: {pages}";
+
+//            Console.WriteLine(sentence);
+//        }
+//    }
+//}
+
+//QUESTION 7
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string title = "Clean Code";
+//            int pages = 464;
+
+//            string sentence = string.Format("Book: {0}, Pages: {1}", title, pages);
+
+//            Console.WriteLine(sentence);
+//        }
+//    }
+//}
+
+//QUESTION 8
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            int pages = 464;
+
+//            if (pages > 300)
+//            {
+//                Console.WriteLine("Long Book");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Short Book");
+//            }
+//        }
+//    }
+//}
+
+//QUESTION 9
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            int pages = 464;
+//            bool isAvailable = true;
+
+//            if (pages > 300 && isAvailable)
+//            {
+//                Console.WriteLine("You can borrow this book");
+//            }
+//        }
+//    }
+//}
+
+//QUESTION 10
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string title = "Refactoring";
+
+//            switch (title)
+//            {
+//                case "Clean Code":
+//                    Console.WriteLine("Great choice!");
+//                    break;
+
+//                case "Refactoring":
+//                    Console.WriteLine("Nice pick!");
+//                    break;
+
+//                default:
+//                    Console.WriteLine("Never heard of it");
+//                    break;
+//            }
+//        }
+//    }
+//}
+
+//QUESTION 11
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            int pages = 464;
+
+//            string sizeLabel = pages > 300 ? "Long Book" : "Short Book";
+
+//            Console.WriteLine(sizeLabel);
+//        }
+//    }
+//}
+
+//QUESTION 12
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+
+//            for (int i = 0; i < books.Length; i++)
+//            {
+//                Console.WriteLine((i + 1) + ". " + books[i]);
+//            }
+//        }
+//    }
+//} 
+
+//QUESTION 13
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+
+//            int i = 0; while (i < books.Length)
+
+//            {
+//                Console.WriteLine(books[i]);
+//                i++;
+//            }
+//        }
+//    }
+//}
+
+//QUESTION 14
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main(string[] args)
+//        {
+//            int count = 0;
+
+//            do {Console.WriteLine("Checking book...");
+//                count++;}
+
+//            while (count < 3);
+//        }
+//    }
+//}
+//QUESTION 15
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+
+//            foreach (string book in books)
+//            {
+//                Console.WriteLine(book);
+//            }
+//        }
+//    }
+//}
+
+//QUESTION 16
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+
+//            foreach (string book in books)
+//            {
+//                if (book == "Refactoring")
+//                {
+//                    break;
+//                }
+
+//                Console.WriteLine(book);
+//            }
+//        }
+//    }
+//}
+
+//QUESTION 17
+
+//namespace C_Basic01G02
+//{
+//    internal class Assingment2
+//    {
+//        static void Main()
+//        {
+//            string[] books = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+
+//            foreach (string book in books)
+//            {
+//                if (book == "The Pragmatic Programmer")
+//                {
+//                    continue;
+//                }
+
+//                Console.WriteLine(book);
+//            }
+//        }
+//    }
+//}
---
+++ C#Basic01G02/Program.cs
@@ -0,0 +1,205 @@
+﻿
+// FIRST QUESTION 
+
+//namespace C_Basic01G02
+//{
+//    public class Book
+//    {
+//        public override string ToString()
+//        {
+//            return $"{Title} - {Pages} pages";
+//        }
+//        public string Title { get; set; }
+//        public int Pages { get; set; }
+//    }
+//    class Program
+//    {
+//        static void Main()
+//        {
+//            object myBook = new Book { Title = "The Hobbit", Pages = 310 };
+
+//            Console.WriteLine(myBook);
+//        }
+//    }
+//}
+
+// SECOND QUSTION
+
+//namespace C_Basic01G02
+//{
+//    internal class Book
+//    {
+//              public override string ToString()
+//              {
+//                  return $"{Title} - {Pages} pages";
+//              }
+
+//        public string Title { get; set; }
+//        public int Pages { get; set; }
+//    }
+
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            Book book = new Book { Title = "The Hobbit", Pages = 310 };
+
+//            Console.WriteLine(book.ToString());
+//            Console.WriteLine(book.Equals(book));
+//            Console.WriteLine(book.GetHashCode());
+//            Console.WriteLine(book.GetType());
+//        }
+//    }
+//}
+
+//THIRD QUSTION 
+
+//namespace C_Basic01G02
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            int numerator = 10;
+//            int denominator = 0;
+//            try
+//            {
+//                int result = numerator / denominator;
+//                Console.WriteLine(result);
+//            }
+//            catch (Exception )
+//            {
+//                Console.WriteLine("Cannot divide by zero");
+//            }
+//            finally
+//            {
+//                Console.WriteLine("Done");
+//            }
+//        }
+//    }
+//}
+
+// FOURTH QUESTION 
+
+//namespace C_Basic01G02
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            int pages = 300;
+//            double pagesAsDouble = pages;
+
+//            Console.WriteLine(pagesAsDouble);
+//        }
+//    }
+//}
+
+// QUESTION 5 
+
+//namespace C_Basic01G02
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            double price = 49.99;
+//            int priceAsInt = (int)price;
+
+//            Console.WriteLine(priceAsInt);
+//        }
+//    }
+//}
+
+// QUESTION 6
+
+//namespace C_Basic01G02
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            string pagesText = "464";
+//            int pages = Convert.ToInt32(pagesText);
+
+//            Console.WriteLine(pages);
+//        }
+//    }
+//}
+
+// QUESTION 7
+
+//namespace C_Basic01G02
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            string yearText = "2023";
+//            int year = int.Parse(yearText);
+//            Console.WriteLine(year);
+
+//           
+//            string badText = "abc";
+//            bool success = int.TryParse(badText, out int result);
+//            if (success)
+//            {
+//                Console.WriteLine(result);
+//            }
+//            else
+//            {
+//                Console.WriteLine("Invalid number");
+//            }
+//        }
+//    }
+//}
+
+// QUESTION 8
+
+//namespace C_Basic01G02
+//{
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            int pages = 464;
+//            string pagesText = pages.ToString();
+
+//            Console.WriteLine(pagesText);
+//            Console.WriteLine(pagesText.GetType());
+//        }
+//    }
+//}
+
+//namespace C_Basic01G2 {
+
+
+//    internal class C_Basic01G2 {
+     
+//            public int TotalGrad = 0;
+//            public int AverageGrade = TotalGrad \ 4;
+
+//        static void Main() {
+
+//            Console.WriteLine(" Enter Your Name ");
+//            string student = Console.ReadLine();
+
+//            Console.WriteLine(" Enter Your Age ");
+//            int.TryParse(Console.ReadLine() , out int age);
+//            Console.WriteLine(" Enter Your Grade 1 ");
+//            double.TryParse(Console.ReadLine(), out double grade1);
+//            Console.WriteLine(" Enter Your Grade 2");
+//            double.TryParse(Console.ReadLine(), out double grade2);
+
+//            Console.WriteLine(" Enter Your Grade 3");
+//            double.TryParse(Console.ReadLine(), out double grade3);
+
+
+
+            
+//        }
+
+//    }
+
+//}
+
